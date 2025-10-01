dfe92bee8e79c312db4943ace9ae53bb826823ab
bini <7975099@hanmail.net>
2025-10-01 16:19:38 +0900

    Stepper 컴포넌트 생성 테스트

---
+++ my-schedule/src/app/components/Stepper.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useState } from "react";
+
+const steps = ['정보 입력', '검토', '완료']
+
+export default function Stepper(){
+    const [currentStep, setCurrentStep] = useState<number>(0);
+
+    const nextStep = () => {
+        if(currentStep < steps.length - 1){
+            setCurrentStep(currentStep + 1);
+        }
+    };
+
+    const prevStep = () => {
+        if(currentStep > 0){
+            setCurrentStep(currentStep - 1);
+        }
+    };
+
+    const progressPercent = ((currentStep + 1) / steps.length) * 100;
+
+    return(
+        <div>
+            <div>
+        
+            </div>
+        </div>
+    )
+}
